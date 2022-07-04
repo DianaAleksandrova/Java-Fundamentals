31957d875a84b24ff7439b4fb94d670e54cb0153
DianaAleksandrova <104830664+DianaAleksandrova@users.noreply.github.com>
2022-07-04 15:19:17 +0300

    Added a source code

---
+++ DataTypesAndVariablesLab/PO2PoundsToDollars.java
@@ -0,0 +1,12 @@
+package DataTypesAndVariablesLaab;
+
+import java.util.Scanner;
+
+public class PO2PoundsToDollars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double pounds = Double.parseDouble(scanner.nextLine());
+        double dollars = pounds * 1.31;
+        System.out.printf("%.3f",dollars);
+    }
+}
---
+++ DataTypesAndVariablesLab/PO6CharsToString.java
@@ -0,0 +1,10 @@
+package DataTypesAndVariablesLaab;
+
+import java.util.Scanner;
+
+public class PO6CharsToString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
